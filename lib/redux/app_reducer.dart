395441f39bb0aaf6e1cc8937525baba4902fd317
VicBVic <0.botnaruvictor.0@gmail.com>
2022-11-25 17:48:49 +0200

    init redux

---
+++ lib/redux/app_reducer.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:qed/redux/app_reducer.dart';
+import 'package:qed/redux/app_state.dart';
+
+AppState appReducer(AppState state, dynamic action) {
+  return state;
+}
---
+++ lib/redux/app_state.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_redux/flutter_redux.dart';
+
+class QedUser {
+  //TODO: sterge smecheria
+}
+
+class Contest {
+  //TODO: sterge
+}
+
+class Problems {
+  //TODO: sterge
+}
+
+class AppState {
+  QedUser? currentUser;
+  Set<Contest> contests = Set();
+  Set<Problems> problems = Set();
+}
