ffe0907ddb16b9073f022ededbc6b67a0fd3fe7e
VicBVic <0.botnaruvictor.0@gmail.com>
2022-11-26 15:41:32 +0200

    sing up sorta kinda finished

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:qed/firebase/qedstore.dart';
+import 'package:qed/screens/signin.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -9,6 +11,26 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  var hintMap = {
+    0: 'name',
+    1: 'nickname',
+    2: 'email',
+    3: 'password',
+    4: 'confirm password'
+  };
+  final int fieldNum = 5;
+
+  final _formKey = GlobalKey<FormState>();
+
+  late List<TextEditingController> controllers;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    controllers = List.filled(fieldNum, TextEditingController());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,10 +44,38 @@ class _SignUpState extends State<SignUp> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
-                    onPressed: () async =>
-                        await QEDStore.instance.singInWithGoogle(),
-                    child: Text("Continue with google"))
+                ...List.generate(
+                  fieldNum,
+                  (index) => TextFormField(
+                    controller: controllers[index],
+                    decoration: InputDecoration(
+                      hintText: hintMap[index],
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Sign up"),
+                ),
+                ElevatedButton(
+                  onPressed: () => Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignIn(),
+                    ),
+                    (a) => false,
+                  ),
+                  child: Text("Return"),
+                ),
+                Text("or"),
+                SignInButton(
+                  Buttons.GoogleDark,
+                  onPressed: () {
+                    QEDStore.instance.singInWithGoogle();
+                  },
+                  padding: EdgeInsets.all(16.0),
+                  elevation: 20.0,
+                ),
               ],
             ),
             flex: 2,
