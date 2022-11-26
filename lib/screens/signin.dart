ebd8de3e7ed84b165bacdba6d5c35956bf385d2a
VicBVic <0.botnaruvictor.0@gmail.com>
2022-11-26 13:56:31 +0200

    made signin with google

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -14,60 +14,76 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  TextEditingController passController = TextEditingController();
+  TextEditingController userController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            FittedBox(
-                child: Text("Welcome to QED!",
-                    style: Theme.of(context).textTheme.headline2)),
-            TextField(
-              decoration: InputDecoration(hintText: "Username"),
-            ),
-            TextField(
-              decoration: InputDecoration(hintText: "Password"),
-              obscureText: true,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SignInButton(
-                Buttons.Email,
-                onPressed: () {},
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FittedBox(
+                  child: Text("Welcome to QED!",
+                      style: Theme.of(context).textTheme.headline2)),
+              TextField(
+                decoration: InputDecoration(hintText: "Username"),
+                controller: userController,
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: "Password"),
+                controller: passController,
+                obscureText: true,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SignInButton(
+                  Buttons.Email,
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text(
+                                'Processing Data ${userController.text} ${passController.text}')),
+                      );
+                    }
+                  },
+                  padding: EdgeInsets.all(16.0),
+                  elevation: 20.0,
+                ),
+              ),
+              Divider(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Or sign with an external platform:"),
+              ),
+              SignInButton(
+                Buttons.GoogleDark,
+                onPressed: () {
+                  QEDStore.instance.singInWithGoogle();
+                },
                 padding: EdgeInsets.all(16.0),
                 elevation: 20.0,
               ),
-            ),
-            Divider(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Or sign with an external platform:"),
-            ),
-            SignInButton(
-              Buttons.GoogleDark,
-              onPressed: () {
-                QEDStore.instance.singInWithGoogle();
-              },
-              padding: EdgeInsets.all(16.0),
-              elevation: 20.0,
-            ),
-            Divider(),
-            TextButton(
-              child: Text(" New user? Sign up here!"),
-              onPressed: () => Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SignUp(),
+              Divider(),
+              TextButton(
+                child: Text(" New user? Sign up here!"),
+                onPressed: () => Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SignUp(),
+                  ),
+                  (a) => false,
                 ),
-                (a) => false,
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
