4e0194e8fb52052bd97df0dc24454d10bc4c79e5
VicBVic <0.botnaruvictor.0@gmail.com>
2022-11-26 13:33:25 +0200

    signin frontend

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class App extends StatelessWidget {
             if (vm.state.currentUser != null) {
               return HomeScreen();
             }
-            return SignUp();
+            return SignIn();
           })),
           routes: {
             '/home': (context) => HomeScreen(),
--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:qed/main.dart';
+import 'package:qed/screens/signup.dart';
+
+import '../firebase/qedstore.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -11,7 +17,59 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(),
+      //appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FittedBox(
+                child: Text("Welcome to QED!",
+                    style: Theme.of(context).textTheme.headline2)),
+            TextField(
+              decoration: InputDecoration(hintText: "Username"),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Password"),
+              obscureText: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SignInButton(
+                Buttons.Email,
+                onPressed: () {},
+                padding: EdgeInsets.all(16.0),
+                elevation: 20.0,
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Or sign with an external platform:"),
+            ),
+            SignInButton(
+              Buttons.GoogleDark,
+              onPressed: () {
+                QEDStore.instance.singInWithGoogle();
+              },
+              padding: EdgeInsets.all(16.0),
+              elevation: 20.0,
+            ),
+            Divider(),
+            TextButton(
+              child: Text(" New user? Sign up here!"),
+              onPressed: () => Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SignUp(),
+                ),
+                (a) => false,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
